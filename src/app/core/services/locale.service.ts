ba85ca54b969feb44846ace717bd34fefacf1a9a
Alberto <albertomorenogonzalez03@gmail.com>
2023-02-25 19:11:02 +0100

    Solución con los commits del proyecto

---
+++ src/app/core/services/locale.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+ 
+@Injectable({providedIn:'root'})
+export class LocaleService {
+ 
+ //Chosse Locale From This Link
+ //https://github.com/angular/angular/tree/master/packages/common/locales
+ constructor() { }
+ 
+ private _locale: string = '';  
+ 
+ set locale(value: string) {
+   this._locale = value;
+ }
+ get locale(): string {
+   return this._locale || 'es';
+ }
+ 
+ public registerCulture(culture: string) {
+   if (!culture) {
+     return;
+   }
+   switch (culture) {
+     case 'es':
+     case 'es-es':
+       this._locale = 'es-es';
+       break;  
+    
+     case 'en-us':
+       this._locale = 'en-us';
+         break;
+     case 'gb':
+     case 'en':
+     case 'en-uk':
+       this._locale = 'en-uk';
+       break;
+    
+     default: {
+       this._locale = 'en-uk';
+       break;
+     }
+    }
+  }  
+}
